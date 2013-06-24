f07cc74e2a3896c4f70b9ba6b9206c3f1811c64e
dewey <mail@notmyhostna.me>
2013-06-25 00:00:12 +0200

    Some settings

--- src/form/controller/InputServlet.java
+++ src/form/controller/InputServlet.java
@@ -1,20 +1,13 @@
 package form.controller;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletOutputStream;
 import java.io.IOException;
 
-/**
- * Created with IntelliJ IDEA.
- * User: philipp
- * Date: 6/24/13
- * Time: 11:14 PM
- * To change this template use File | Settings | File Templates.
- */
 @WebServlet(name = "InputServlet")
 public class InputServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
